a66e5939ae84a86b042d76d18a40e844ad5efc50
A L P H A <saikatmahara7895@gmail.com>
2025-07-29 22:00:24 +0530

    Add files via upload

---
+++ Week 2/Week 2-Q3.py
@@ -0,0 +1,19 @@
+def printDiagonalSums(mat, n):
+    principal = 0
+    secondary = 0;
+    for i in range(0, n): 
+        for j in range(0, n): 
+            if (i == j):
+                principal += mat[i][j]
+            if ((i + j) == (n - 1)):
+                secondary += mat[i][j]       
+    print("Principal Diagonal:", principal)
+    print("Secondary Diagonal:", secondary)
+a = [[ 11, 22, 33, 44 ],
+     [ 55, 66, 77, 88 ], 
+     [ 10, 20, 31, 42 ],
+      [ 5, 6, 77, 80 ]]
+printDiagonalSums(a, 4)
+# Output :- 
+# Principal Diagonal: 188
+# Secondary Diagonal: 146
