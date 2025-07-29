b8f1a1ddd1ee91fa128c79e09251d2312e57bb6a
A L P H A <saikatmahara7895@gmail.com>
2025-07-29 21:43:22 +0530

    Delete Week1-Q8.py

--- Week1-Q8.py
+++
@@ -1,9 +0,0 @@
-s = "UEM"
-rev = ""  
-for char in s:
-    rev = char + rev
-if s == rev:
-    print("Yes the given expression was Palindrome")
-else:
-    print("No the given expression was not Palindrome")
-#Output :- No the given expression was not Palindrome
