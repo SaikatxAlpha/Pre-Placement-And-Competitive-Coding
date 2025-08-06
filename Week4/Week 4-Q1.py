1aef3a623c9a33dfbb633536de30806a20f44274
A L P H A <saikatmahara7895@gmail.com>
2025-08-06 15:30:15 +0530

    Add files via upload

---
+++ Week4/Week 4-Q1.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, new_data):
+        self.data = new_data
+        self.next = None
+        self.prev = None
+def insert_at_front(head, new_data):
+    new_node = Node(new_data)
+    new_node.next = head
+    if head is not None:
+        head.prev = new_node
+    return new_node
+def print_list(head):
+    curr = head
+    while curr is not None:
+        print(f" {curr.data}", end='')
+        curr = curr.next
+    print()  
+if __name__ == "__main__":
+    head = Node(2)
+    head.next = Node(3)
+    head.next.prev = head
+    head.next.next = Node(4)
+    head.next.next.prev = head.next
+    print("Original Linked List:", end='')
+    print_list(head)
+    print("After inserting Node at the front:", end='')
+    data = 1
+    head = insert_at_front(head, data)
+    print_list(head)
